3ef241dd5766bb00d960cd8bff3f2fe1387f2d16
venkateshbabualamkaram <venkateshbabualamkaram@gmail.com>
2026-02-22 10:28:51 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,7 +1,7 @@
 public demo{
     public static void main(String[] args) {
         System.out.println("hello world!!!");
-
+        int a=10;
         System.out.println("hi this is venkatesh");
     }
 }
